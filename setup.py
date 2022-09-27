610fc5f233566ce29eb87fafebdbb08d5e356969
Jordan Last <jordan.michael.last@gmail.com>
2022-09-27 18:16:06 -0500

    release 0.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from distutils.core import setup
 
 setup(
     name='kybra',
-    version='0.0.3',
+    version='0.0.4',
     package_data={
         '': ['compiler/**']
     },
     include_package_data=True,
     packages=['kybra']
 )
-
-# TODO seems like I have to do this to upload a new version
