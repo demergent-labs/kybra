1127cfc8d5d38c2bc4f599731ebd19a65c8664c9
Jordan Last <jordan.michael.last@gmail.com>
2022-09-27 18:12:37 -0500

    test

--- kybra/__main__.py
+++ kybra/__main__.py
@@ -4,5 +4,6 @@ import subprocess
 import sys
 
 compiler_path = os.path.dirname(kybra.__file__) + '/compiler'
+build_sh_path = compiler_path + '/build.sh'
 
-subprocess.call([compiler_path + '/build.sh', sys.argv[1], sys.argv[2], sys.argv[3], compiler_path])
+subprocess.call([build_sh_path, sys.argv[1], sys.argv[2], sys.argv[3], compiler_path])
