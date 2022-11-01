b2b6f695320099bc2d6ab17e5528d616c9ebf053
Jordan Last <jordan.michael.last@gmail.com>
2022-11-01 12:05:33 -0500

    fix notify raw result type

--- examples/notify_raw/test/dfx_generated/canister1/canister1.did.d.ts
+++ examples/notify_raw/test/dfx_generated/canister1/canister1.did.d.ts
@@ -1,8 +1,15 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export type RejectionCode = { 'NoError' : null } |
+  { 'CanisterError' : null } |
+  { 'SysTransient' : null } |
+  { 'DestinationInvalid' : null } |
+  { 'Unknown' : null } |
+  { 'SysFatal' : null } |
+  { 'CanisterReject' : null };
 export type SendNotificationResult = { 'ok' : boolean } |
-  { 'err' : string };
+  { 'err' : RejectionCode };
 export interface _SERVICE {
   'send_notification' : ActorMethod<[], SendNotificationResult>,
 }
