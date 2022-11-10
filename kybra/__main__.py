4b63e15bf29f40aec7a2d2f15f7b27baa234551e
Dan Steren <dan@dansteren.com>
2022-11-09 17:51:01 -0700

    Ignore failure to install ic-cdk-optimizer
    When installing the ic-cdk-optimizer in CI/CD without the `--force` flag
    we get the following output:
    
    ```shell
    [1/3] 🔨 Compiling Python...
    
    💣 Unable to install dependency "ic-cdk-optimizer"
    
        Updating crates.io index
    error: binary `ic-cdk-optimizer` already exists in destination
    Add --force to overwrite
    
    💀 Build failed
    ```
    
    When using `--force` to install the optimizer, all dependencies of
    ic-cdk-optimizer are re-installed every time. This means that step 1 of
    the script jumps from 1 second up to 90 seconds on subsequent installs.
    
    Rather than force everyone to wait for all dependencies to re-install
    each time they run `dfx deploy`, we now assume that a failure means the
    binary was already installed.
    
    ## Why does it fail?
    
    It seems like the cache in CI/CD doesn't contain everything it needs.
    Some minimal research makes me believe we need to cache:
    
    - ~/.cargo/.crates.toml
    - ~/.cargo/.crates2.json
    
    See:
    
    https://www.reddit.com/r/learnrust/comments/lexewr/the_difference_in_behaviour_when_running_cargo/

--- kybra/__main__.py
+++ kybra/__main__.py
@@ -158,7 +158,7 @@ def compile_python_or_exit(
 ):
     bundle_python_code(paths)
     add_wasm_compilation_target_or_exit(verbose)
-    install_ic_cdk_optimizer_or_exit(verbose)
+    install_ic_cdk_optimizer(verbose)
     run_kybra_generate_or_exit(paths, cargo_env, verbose)
     run_rustfmt_or_exit(paths, verbose)
 
@@ -243,21 +243,21 @@ def add_wasm_compilation_target_or_exit(verbose: bool = False):
         sys.exit(1)
 
 
-def install_ic_cdk_optimizer_or_exit(verbose: bool = False):
+def install_ic_cdk_optimizer(verbose: bool = False):
     # TODO this should eventually be replaced with ic-wasm once this is resolved: https://forum.dfinity.org/t/wasm-module-contains-a-function-that-is-too-complex/15407/43?u=lastmjs
     install_cdk_optimizer_result = subprocess.run(
-        ["cargo", "install", "ic-cdk-optimizer", "--version=0.3.4", "--force"],
+        ["cargo", "install", "ic-cdk-optimizer", "--version=0.3.4"],
         capture_output=not verbose,
     )
     # install_ic_wasm_result = subprocess.run(
     #     ["cargo", "install", "ic-wasm", "--version=0.3.0"], capture_output=not verbose
     # )
 
+    # Assume that a failure means it's already installed
     if install_cdk_optimizer_result.returncode != 0:
-        print(red('\n💣 Unable to install dependency "ic-cdk-optimizer"\n'))
+        print(red('\n Warning: unable to install dependency "ic-cdk-optimizer"\n'))
         print(install_cdk_optimizer_result.stderr.decode("utf-8"))
-        print("💀 Build failed")
-        sys.exit(1)
+        print("proceeding anyway...")
 
 
 def run_kybra_generate_or_exit(paths: Paths, cargo_env: dict[str, str], verbose: bool):
