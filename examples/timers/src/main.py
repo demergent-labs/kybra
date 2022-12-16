b3b1f9881373148116653bdc57b5993cea24d307
Dan Steren <dan@dansteren.com>
2022-12-16 15:06:27 -0700

    Nix lambda support in timers
    Lambdas feel super janky and un-pythonic here. The example felt super
    contrived.
    
    Adding lambda support greatly complicates things and since the gain
    feels minute, I'm nixing support for it for now.

--- examples/timers/src/main.py
+++ examples/timers/src/main.py
@@ -3,19 +3,15 @@ from kybra import Duration, ic, nat8, query, Record, TimerId, update
 
 class StatusReport(Record):
     single: bool
-    inline1: nat8
-    inline2: nat8
     repeat: nat8
 
 
 class TimerIds(Record):
     single: TimerId
-    inline1: TimerId
-    inline2: TimerId
     repeat: TimerId
 
 
-status: StatusReport = {"single": False, "inline1": 0, "inline2": 0, "repeat": 0}
+status: StatusReport = {"single": False, "repeat": 0}
 
 
 @update
@@ -26,29 +22,15 @@ def clear_timer(timer_id: TimerId):
 
 @update
 def set_timers(delay: Duration, interval: Duration) -> TimerIds:
+    # Note: Lambdas aren't supported. You can only pass a function here.
     single_id = ic.set_timer(delay, one_time_timer_callback)
 
-    # Note: Demergent Labs does not recommend using lambdas in timers. They will
-    # be called, but they are limited in functionality. Calling a defined
-    # function is preferred whenever possible.
+    ic.print(f"Set timer {single_id}")
 
-    # Note: You cannot set global variables from within a lambda but you can
-    # call a function that sets a global variable. So we've moved the "setting"
-    # functionality out into helper functions while the printing is kept here in
-    # the lambda.
-
-    # inline1_id = ic.set_timer(
-    #     delay, lambda: update_status_report_inline_1() or print("Inline timer 1 called")
-    # )
-    # inline2_id = ic.set_timer(
-    #     delay, lambda: update_status_report_inline_1() or print("Inline timer 2 called")
-    # )
     # repeat_id = ic.set_timer_interval(interval, repeat_timer_callback)
 
     return {
         "single": single_id,
-        "inline1": 0,
-        "inline2": 0,
         "repeat": 0,
     }
 
@@ -61,20 +43,10 @@ def status_report() -> StatusReport:
 def one_time_timer_callback():
     global status
     status["single"] = True
-    print("one_time_timer_callback called")
+    ic.print("one_time_timer_callback called")
 
 
 def repeat_timer_callback():
     global status
     status["repeat"] += 1
-    print(f"Repeating timer. Call ${status['repeat']}")
-
-
-def update_status_report_inline_1():
-    global status
-    status["inline1"] = 1
-
-
-def update_status_report_inline_2():
-    global status
-    status["inline1"] = 2
+    ic.print(f"Repeating timer. Call {status['repeat']}")
