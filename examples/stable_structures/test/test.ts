37e6eeb517670e7ce35febbf685ea7b57a869b03
Benjamin DeMann <benjamin.demann@gmail.com>
2023-01-17 11:36:02 -0700

    separate into three canisters

--- examples/stable_structures/test/test.ts
+++ examples/stable_structures/test/test.ts
@@ -7,6 +7,7 @@ import {
 } from 'azle/examples/stable_structures/test/tests';
 import { createActor as createActorCanister1 } from './dfx_generated/canister1';
 import { createActor as createActorCanister2 } from './dfx_generated/canister2';
+import { createActor as createActorCanister3 } from './dfx_generated/canister3';
 
 const stable_structures_canister_1 = createActorCanister1(
     'rrkah-fqaaa-aaaaa-aaaaq-cai',
@@ -15,6 +16,11 @@ const stable_structures_canister_1 = createActorCanister1(
 
 const stable_structures_canister_2 = createActorCanister2(
     'ryjl3-tyaaa-aaaaa-aaaba-cai',
+    { agentOptions: { host: 'http://127.0.0.1:8000' } }
+);
+
+const stable_structures_canister_3 = createActorCanister3(
+    'r7inp-6aaaa-aaaaa-aaabq-cai',
     { agentOptions: { host: 'http://127.0.0.1:8000' } }
 );
 
@@ -37,8 +43,9 @@ const tests: Test[] = [
             });
         }
     },
-    ...pre_redeploy_tests(stable_structures_canister_1 as any, 0, 6),
-    ...pre_redeploy_tests(stable_structures_canister_2 as any, 7, 13),
+    ...pre_redeploy_tests(stable_structures_canister_1 as any, 0, 4),
+    ...pre_redeploy_tests(stable_structures_canister_2 as any, 5, 9),
+    ...pre_redeploy_tests(stable_structures_canister_3 as any, 10, 13),
     {
         name: 'redeploy canisters',
         prep: async () => {
@@ -49,11 +56,12 @@ const tests: Test[] = [
             });
         }
     },
-    ...post_redeploy_tests(stable_structures_canister_1 as any, 0, 6),
-    ...post_redeploy_tests(stable_structures_canister_2 as any, 7, 13),
+    ...post_redeploy_tests(stable_structures_canister_1 as any, 0, 4),
+    ...post_redeploy_tests(stable_structures_canister_2 as any, 5, 9),
+    ...post_redeploy_tests(stable_structures_canister_3 as any, 10, 13),
     ...insert_error_tests(
         stable_structures_canister_1 as any,
-        stable_structures_canister_2 as any
+        stable_structures_canister_3 as any
     )
 ];
 
