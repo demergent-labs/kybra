51bedbc57bf0a0321122f2566ce41f327c9a9290
Jordan Last <jordan.michael.last@gmail.com>
2023-01-20 13:05:20 -0600

    add more robust local Rust installation process

--- kybra/compiler/install_rustup.sh
+++ kybra/compiler/install_rustup.sh
@@ -16,14 +16,26 @@ global_kybra_target_dir=$global_kybra_config_dir/target
 export CARGO_HOME=$global_kybra_config_dir
 export RUSTUP_HOME=$global_kybra_config_dir
 
+function install_wasm32_unknown_unknown() {
+    $global_kybra_rustup_bin target add wasm32-unknown-unknown &> $global_kybra_logs_dir/step_2_wasm32_unknown_unknown
+}
+
+function install_ic_wasm() {
+    $global_kybra_cargo_bin install ic-wasm --version 0.3.0 &> $global_kybra_logs_dir/step_3_ic_wasm
+}
+
+function install_ic_cdk_optimizer() {
+    $global_kybra_cargo_bin install ic-cdk-optimizer --version 0.3.4  &> $global_kybra_logs_dir/step_4_ic_cdk_optimizer
+}
+
 if [ -x "$(command -v $global_kybra_rustup_bin)" ]; then
     $global_kybra_rustup_bin update $rust_version &> $global_kybra_logs_dir/rustup_update
 else
-    echo -e "Keep calm, this could take a few minutes. Subsequent installs will be faster.\n"
+    echo -e "\nKybra $kybra_version initial installation (this takes a few minutes)\n"
 
     mkdir -p ~/.config/kybra
-    mkdir $global_kybra_config_dir
-    mkdir $global_kybra_logs_dir
+    mkdir -p $global_kybra_config_dir
+    mkdir -p $global_kybra_logs_dir
 
     echo -e "1/4) Installing Rust"
 
@@ -31,13 +43,31 @@ else
 
     echo -e "2/4) Installing wasm32-unknown-unknown"
 
-    $global_kybra_rustup_bin target add wasm32-unknown-unknown &> $global_kybra_logs_dir/step_2_wasm32_unknown_unknown
+    install_wasm32_unknown_unknown
 
     echo -e "3/4) Installing ic-wasm"
 
-    $global_kybra_cargo_bin install ic-wasm --version 0.3.0 &> $global_kybra_logs_dir/step_3_ic_wasm
+    install_ic_wasm
 
-    echo -e "4/4) Installing ic-cdk-optimizer"
+    echo -e "4/4) Installing ic-cdk-optimizer\n"
 
-    $global_kybra_cargo_bin install ic-cdk-optimizer --version 0.3.4  &> $global_kybra_logs_dir/step_4_ic_cdk_optimizer
+    install_ic_cdk_optimizer
+fi
+
+if ! $global_kybra_rustup_bin target list | grep -q "wasm32-unknown-unknown (installed)"; then
+    echo -e "\nKybra $kybra_version is installing wasm32-unknown-unknown\n"
+
+    install_wasm32_unknown_unknown
+fi
+
+if ! test -e $global_kybra_bin_dir/ic-wasm; then
+    echo -e "\nKybra $kybra_version is installing ic-wasm\n"
+
+    install_ic_wasm
+fi
+
+if ! test -e $global_kybra_bin_dir/ic-cdk-optimizer; then
+    echo -e "\nKybra $kybra_version is installing ic-cdk-optimizer\n"
+
+    install_ic_cdk_optimizer
 fi
