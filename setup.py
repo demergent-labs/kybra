db90d719518e6db78199a3f064df076df57014c4
Jordan Last <jordan.michael.last@gmail.com>
2023-01-24 14:28:26 -0600

    fix the type stub missing error

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup
 from kybra import __version__
 
 setup(
-    name='kybra',
+    name="kybra",
     version=__version__,
-    package_data={
-        'kybra': ['compiler/**', 'canisters/**']
-    },
+    package_data={"kybra": ["compiler/**", "canisters/**", "py.typed"]},
     include_package_data=True,
-    packages=['kybra'],
-    install_requires=['modulegraph==0.19.3']
+    packages=["kybra"],
+    install_requires=["modulegraph==0.19.3"],
 )
