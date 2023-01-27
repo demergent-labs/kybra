78eaf271320c4f1b6fe3a2ab3ed42574fc942f9f
Dan Steren <dan@dansteren.com>
2023-01-27 11:18:24 -0700

    Organize header

--- kybra/compiler/kybra_generate/src/generators/header/use_statements.rs
+++ kybra/compiler/kybra_generate/src/generators/header/use_statements.rs
@@ -1,7 +1,5 @@
 pub fn generate() -> proc_macro2::TokenStream {
     quote::quote! {
-        #![allow(warnings, unused)]
-
         use rustpython_vm::{AsObject, builtins::{PyDict, PyBaseException, PyGenerator, PyListRef, PyTupleRef, PyIntRef, PyStr, PyList, PyTuple, PyBytes}, class::PyClassImpl, convert::ToPyObject, function::IntoFuncArgs, PyObjectRef, PyObject, PyRef, VirtualMachine, protocol::{PyIter, PyIterReturn}, py_serde::{deserialize, serialize}};
         use rustpython_derive::{pyclass, PyPayload};
         use kybra_vm_value_derive::{CdkActTryIntoVmValue, CdkActTryFromVmValue};
@@ -12,16 +10,5 @@ pub fn generate() -> proc_macro2::TokenStream {
         use slotmap::Key;
         use rand::{Rng, SeedableRng, rngs::StdRng};
         use std::convert::TryInto;
-
-        thread_local! {
-            static RNG_REF_CELL: std::cell::RefCell<StdRng> = std::cell::RefCell::new(SeedableRng::from_seed([0u8; 32]));
-        }
-
-        static mut _KYBRA_INTERPRETER_OPTION: Option<rustpython_vm::Interpreter> = None;
-        static mut _KYBRA_SCOPE_OPTION: Option<rustpython_vm::scope::Scope> = None;
-
-        // TODO this is broken https://github.com/dfinity/motoko/issues/3462#issuecomment-1260060874
-        // #[link_section = "icp:public cdk"]
-        // pub static NAME: [u8; 12] = *b"kybra v0.0.0";
     }
 }
