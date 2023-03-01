a80e446016afa114a02b5abdbea3144fd93891c6
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-01 15:31:45 -0700

    remove generated file from pre_and_post_upgrade

--- examples/pre_and_post_upgrade/test/test.ts
+++ examples/pre_and_post_upgrade/test/test.ts
@@ -1,5 +1,4 @@
-import { run_tests, Test } from 'azle/test';
-import { execSync } from 'child_process';
+import { run_tests } from 'azle/test';
 import { get_tests } from 'azle/examples/pre_and_post_upgrade/test/tests';
 import { createActor } from './dfx_generated/pre_and_post_upgrade';
 
@@ -9,25 +8,4 @@ const pre_and_post_canister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    {
-        name: 'deploy',
-        prep: async () => {
-            await new Promise((resolve) => setTimeout(resolve, 5000));
-
-            execSync(
-                `dfx canister uninstall-code pre_and_post_upgrade || true`,
-                {
-                    stdio: 'inherit'
-                }
-            );
-
-            execSync(`dfx deploy pre_and_post_upgrade`, {
-                stdio: 'inherit'
-            });
-        }
-    },
-    ...get_tests(pre_and_post_canister)
-];
-
-run_tests(tests);
+run_tests(get_tests(pre_and_post_canister));
