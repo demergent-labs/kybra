3b423569e6d1944b121c419ca924a2cdad75c98b
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-03 11:07:32 -0700

    explicit void return type for calc

--- examples/motoko_examples/calc/test/test.ts
+++ examples/motoko_examples/calc/test/test.ts
@@ -9,22 +9,4 @@ const calc_canister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    {
-        name: 'deploy',
-        prep: async () => {
-            await new Promise((resolve) => setTimeout(resolve, 5000));
-
-            execSync(`dfx canister uninstall-code calc || true`, {
-                stdio: 'inherit'
-            });
-
-            execSync(`dfx deploy calc`, {
-                stdio: 'inherit'
-            });
-        }
-    },
-    ...get_tests(calc_canister as any)
-];
-
-run_tests(tests);
+run_tests(get_tests(calc_canister as any));
