23549654b38ca328acf62f1135d98166b96fe39d
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-03 16:22:58 -0700

    remove param todos

--- kybra/compiler/kybra_generate/src/py_ast/node/param/errors.rs
+++ kybra/compiler/kybra_generate/src/py_ast/node/param/errors.rs
@@ -1,4 +1,4 @@
-use rustpython_parser::ast::{Located, StmtKind};
+use rustpython_parser::ast::{ArgData, Located, StmtKind};
 
 use crate::{
     errors::{CreateMessage, Message},
@@ -8,5 +8,33 @@ use crate::{
 impl SourceMapped<&Located<StmtKind>> {
     pub fn return_type_annotation_required_error(&self) -> Vec<Message> {
         vec![self.create_error_message("Return type annotation required", "", None)]
+    }
+
+    pub fn dictionary_unpacking_operator_not_supported_error(&self) -> Vec<Message> {
+        let title = "the dictionary unpacking operator (**) is not supported";
+        vec![self.create_error_message(title, "", None)]
+    }
+
+    pub fn iterator_unpacking_operator_not_supported_error(&self) -> Vec<Message> {
+        let title = "the iterator unpacking operator (*) is not supported";
+        vec![self.create_error_message(title, "", None)]
+    }
+
+    pub fn first_parameter_must_be_self_error(&self) -> Vec<Message> {
+        let title = "method must be an instance method. Add \"self\" as the first parameter";
+        vec![self.create_error_message(title, "", None)]
+    }
+
+    pub fn first_parameter_must_not_be_self_error(&self) -> Vec<Message> {
+        let title = "first parameter must not be \"self\"";
+        vec![self.create_error_message(title, "", None)]
+    }
+}
+
+impl SourceMapped<&Located<ArgData>> {
+    pub fn param_type_annotation_required_error(&self) -> Vec<Message> {
+        let param_name = self.node.arg.clone();
+        let title = format!("parameter \"{}\" is missing a type annotation. All method parameters must specify their type.", param_name);
+        vec![self.create_error_message(title.as_str(), "", None)]
     }
 }
--- kybra/compiler/kybra_generate/src/py_ast/node/param/mod.rs
+++ kybra/compiler/kybra_generate/src/py_ast/node/param/mod.rs
@@ -2,7 +2,7 @@ pub mod errors;
 pub mod returns;
 
 use cdk_framework::act::node::param::Param;
-use rustpython_parser::ast::{ArgData, Arguments, Located, StmtKind};
+use rustpython_parser::ast::{ArgData, Located, StmtKind};
 
 use crate::{errors::KybraResult, source_map::SourceMapped};
 
@@ -18,64 +18,45 @@ impl SourceMapped<&Located<StmtKind>> {
     ) -> KybraResult<Vec<Param>> {
         match &self.node {
             StmtKind::FunctionDef { args, .. } => {
-                SourceMapped::new(args.as_ref(), self.source_map.clone())
-                    .to_param_list(internal_or_external)
-            }
-            _ => panic!("Unreachable"),
-        }
-    }
-}
-
-impl SourceMapped<&Arguments> {
-    pub fn to_param_list(
-        &self,
-        internal_or_external: InternalOrExternal,
-    ) -> KybraResult<Vec<Param>> {
-        if self.kwarg.is_some() {
-            todo!(
-                "{}",
-                "the dictionary unpacking operator (**) is not supported".to_string()
-            );
-        }
-
-        if self.vararg.is_some() {
-            todo!(
-                "{}",
-                "the iterator unpacking operator (*) is not supported".to_string()
-            );
-        }
-
-        let param_results = match internal_or_external {
-            InternalOrExternal::Internal => {
-                if self.args[0].node.arg == "self".to_string() {
-                    todo!("{}", "first parameter must not be \"self\"".to_string());
-                }
-                self.args
-                    .iter()
-                    .map(|arg| SourceMapped::new(arg, self.source_map.clone()).to_param())
-                    .collect()
-            }
-            InternalOrExternal::External => {
-                if self.args.len() == 0 {
-                    todo!(
-                        "{}",
-                        "method must be an instance method. Add \"self\" as the first parameter"
-                            .to_string(),
-                    );
+                if args.kwarg.is_some() {
+                    return Err(self.dictionary_unpacking_operator_not_supported_error());
                 }
 
-                if self.args[0].node.arg != "self".to_string() {
-                    todo!("{}", "first parameter must be \"self\"".to_string());
+                if args.vararg.is_some() {
+                    return Err(self.iterator_unpacking_operator_not_supported_error());
                 }
-                self.args[1..]
-                    .iter()
-                    .map(|arg| SourceMapped::new(arg, self.source_map.clone()).to_param())
-                    .collect()
-            }
-        };
 
-        // Ignore the first param, which is always "self"
-        Ok(crate::errors::collect_kybra_results(param_results)?)
+                let param_results = match internal_or_external {
+                    InternalOrExternal::Internal => {
+                        if args.args[0].node.arg == "self".to_string() {
+                            return Err(self.first_parameter_must_not_be_self_error());
+                        }
+                        args.args
+                            .iter()
+                            .map(|arg| SourceMapped::new(arg, self.source_map.clone()).to_param())
+                            .collect()
+                    }
+                    InternalOrExternal::External => {
+                        if args.args.len() == 0 {
+                            return Err(self.first_parameter_must_be_self_error());
+                        }
+
+                        if args.args[0].node.arg != "self".to_string() {
+                            return Err(self.first_parameter_must_be_self_error());
+                        }
+
+                        // Ignore the first param, which is always "self"
+                        args.args[1..]
+                            .iter()
+                            .map(|arg| SourceMapped::new(arg, self.source_map.clone()).to_param())
+                            .collect()
+                    }
+                };
+
+                Ok(crate::errors::collect_kybra_results(param_results)?)
+            }
+            _ => Err(crate::errors::unreachable()),
+        }
     }
 }
 
@@ -84,18 +65,15 @@ impl SourceMapped<&Located<ArgData>> {
         let param_name = self.node.arg.clone();
         match &self.node.annotation {
             Some(annotation) => {
-                let data_type = SourceMapped::new(
-                    annotation.as_ref(),
-                    self.source_map.clone(),
-                )
-                .to_data_type()?;
+                let data_type = SourceMapped::new(annotation.as_ref(), self.source_map.clone())
+                    .to_data_type()?;
 
                 Ok(Param {
                     name: param_name,
                     type_: data_type,
                 })
             }
-            None => todo!("{}", format!("parameter \"{}\" is missing a type annotation. All method parameters must specify their type.", param_name)),
+            None => Err(self.param_type_annotation_required_error()),
         }
     }
 }
