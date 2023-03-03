068fb27114f76d966cbdb16a8e829e38422889fa
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-03 11:37:49 -0700

    void for hello-world, phone-book, simple_to_do

---
+++ examples/motoko_examples/phone-book/test/pretest.ts
@@ -0,0 +1,19 @@
+import { execSync } from 'child_process';
+
+async function pretest() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    execSync(`dfx canister uninstall-code phone_book || true`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx deploy`, {
+        stdio: 'inherit'
+    });
+
+    execSync(`dfx generate`, {
+        stdio: 'inherit'
+    });
+}
+
+pretest();
--- examples/motoko_examples/phone-book/test/test.ts
+++ examples/motoko_examples/phone-book/test/test.ts
@@ -1,6 +1,5 @@
-import { run_tests, Test } from 'azle/test';
+import { run_tests } from 'azle/test';
 import { get_tests } from 'azle/examples/motoko_examples/phone-book/test/tests';
-import { execSync } from 'child_process';
 import { createActor } from './dfx_generated/phone_book';
 
 const phone_book_canister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
@@ -9,22 +8,4 @@ const phone_book_canister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    {
-        name: 'deploy',
-        prep: async () => {
-            await new Promise((resolve) => setTimeout(resolve, 5000));
-
-            execSync(`dfx canister uninstall-code phone_book || true`, {
-                stdio: 'inherit'
-            });
-
-            execSync(`dfx deploy phone_book`, {
-                stdio: 'inherit'
-            });
-        }
-    },
-    ...get_tests(phone_book_canister as any)
-];
-
-run_tests(tests);
+run_tests(get_tests(phone_book_canister as any));
