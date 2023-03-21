09b8624d765442261cf3a991b1c5306cfa6c2211
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-21 13:12:31 -0600

    add reports to the update azle script

--- update_azle.sh
+++ update_azle.sh
@@ -2,12 +2,38 @@
 
 # Instructions: Run this script inside of the examples direcotry
 
-# Traverse the directory structure and find all package.json files, excluding node_modules directories
-while IFS= read -r -d '' dir; do
-    package_json="$dir/package.json"
-    if [[ -f "$package_json" ]] && ! [[ "$package_json" == *"node_modules"* ]] && grep -q "\"azle\"" "$package_json"; then
-        echo "Found azle dependency in $package_json"
-        # Uninstall azle and install the demergent-labs version
-        (cd "$dir" && npm uninstall azle && npm install https://github.com/demergent-labs/azle)
+# function to check if azle is a dependency in package.json
+check_azle_dependency() {
+    if grep -q "\"azle\":" "$1"; then
+        return 0
+    else
+        return 1
     fi
-done < <(find . -type d -not -path '*/node_modules/*' -print0)
+}
+
+updated=0
+updated_folders=()
+checked=0
+
+# loop through all folders that are not inside node_modules
+while IFS= read -r -d '' dir; do
+    if [[ "$dir" != *"node_modules"* ]] && [[ -f "$dir/package.json" ]]; then
+        if check_azle_dependency "$dir/package.json"; then
+            cd "$dir"
+            npm uninstall azle && npm install https://github.com/demergent-labs/azle
+            cd - >/dev/null
+            ((updated++))
+            updated_folders+=("$dir")
+        fi
+        ((checked++))
+        echo "Checked $checked packages..."
+    fi
+done < <(find . -type d -print0)
+
+# generate report
+if [[ "$updated" -eq 0 ]]; then
+    echo "No packages with azle dependency found."
+else
+    echo "Updated azle in $updated package(s):"
+    printf '%s\n' "${updated_folders[@]}"
+fi
