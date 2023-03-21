58db1a7ec982e22f659722b3d8878d9adf966b19
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-21 10:19:17 -0600

    remove dfx_generated from motoko examples

--- examples/motoko_examples/factorial/test/dfx_generated/factorial/factorial.did.js
+++
@@ -1,6 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-    return IDL.Service({ fac: IDL.Func([IDL.Nat], [IDL.Nat], []) });
-};
-export const init = ({ IDL }) => {
-    return [];
-};
--- examples/motoko_examples/persistent-storage/test/dfx_generated/persistent_storage/persistent_storage.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-    get: ActorMethod<[], bigint>;
-    increment: ActorMethod<[], bigint>;
-    reset: ActorMethod<[], bigint>;
-}
