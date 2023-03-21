13037558eafadd1d190187dd48793ddeccf8fb37
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-21 12:50:34 -0600

    second pass

--- update_azle.sh
+++ update_azle.sh
@@ -3,14 +3,15 @@
 # Instructions: Run this script inside of the examples direcotry
 
 # Find all package.json files and loop through them
-find . -name "package.json" -type f | while read package_json; do
+find . -name "package.json" -type f -print0 | while IFS= read -r -d '' package_json; do
     # Check if the azle dependency is present
     if grep -q "\"azle\"" "$package_json"; then
         echo "Found azle dependency in $package_json"
         # Uninstall azle and install the demergent-labs version
-        cd $(dirname "$package_json")
+        dir=$(dirname "$package_json")
+        cd "$dir"
         npm uninstall azle
         npm install https://github.com/demergent-labs/azle
-        cd -
+        cd - > /dev/null
     fi
 done
