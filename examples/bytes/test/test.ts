8a41d888c9cef532680f75cf8089e9d4371e28ec
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-27 15:12:31 -0600

    update bytes tests

--- examples/bytes/test/test.ts
+++ examples/bytes/test/test.ts
@@ -8,10 +8,4 @@ const bytesCanister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-// TODO 2000 kb reaches the instruction limit in RustPython, but not in Azle
-// TODO See if we can get a more efficient conversion for Vec<u8> and bytes
-runTests(
-    getTests(createSnakeCaseProxy(bytesCanister)).filter((test: Test) => {
-        return test.name !== 'get_bytes 2000 kb';
-    })
-);
+runTests(getTests(createSnakeCaseProxy(bytesCanister)));
