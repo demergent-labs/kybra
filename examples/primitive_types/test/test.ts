639d97a35012f99d95714a64824a60b12fa2bfda
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-28 11:11:20 -0600

    update primitive types examples

--- examples/primitive_types/test/test.ts
+++ examples/primitive_types/test/test.ts
@@ -1,4 +1,4 @@
-import { createSnakeCaseProxy, runTests, Test } from 'azle/test';
+import { createSnakeCaseProxy, runTests } from 'azle/test';
 import { getTests } from 'azle/examples/primitive_types/test/tests';
 import { createActor } from './dfx_generated/primitive_types';
 
@@ -8,77 +8,8 @@ const primitiveTypesCanister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    ...getTests(createSnakeCaseProxy(primitiveTypesCanister)),
-    // TODO once Azle has these tests, remove these tests and just use Azle's
-    {
-        name: 'get_text',
-        test: async () => {
-            const result = await primitiveTypesCanister.get_text();
-
-            return {
-                Ok: result === 'this is a string defined with text'
-            };
-        }
-    },
-    {
-        name: 'print_text',
-        test: async () => {
-            const result = await primitiveTypesCanister.print_text(
-                'this is a string defined with text that is also printed'
-            );
-
-            return {
-                Ok:
-                    result ===
-                    'this is a string defined with text that is also printed'
-            };
-        }
-    },
-    {
-        name: 'get_str',
-        test: async () => {
-            const result = await primitiveTypesCanister.get_str();
-
-            return {
-                Ok: result === 'this is a string defined with str'
-            };
-        }
-    },
-    {
-        name: 'print_str',
-        test: async () => {
-            const result = await primitiveTypesCanister.print_str(
-                'this is a string defined with str that is also printed'
-            );
-
-            return {
-                Ok:
-                    result ===
-                    'this is a string defined with str that is also printed'
-            };
-        }
-    },
-    {
-        name: 'get_bool',
-        test: async () => {
-            const result = await primitiveTypesCanister.get_bool();
-
-            return {
-                Ok: result === true
-            };
-        }
-    },
-    {
-        name: 'print_bool',
-        test: async () => {
-            const result = await primitiveTypesCanister.print_bool(false);
-
-            return {
-                Ok: result === false
-            };
-        }
-    }
-];
-
-runTests(tests);
+runTests(
+    getTests(createSnakeCaseProxy(primitiveTypesCanister)).filter(
+        (value) => value.name != 'getNumber' && value.name != 'printNumber'
+    )
+);
