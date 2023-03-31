330b2973475b61e364f130a5605cbcbb54a4cd4a
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-31 15:34:08 -0600

    move get math message into azle

--- examples/imports/test/test.ts
+++ examples/imports/test/test.ts
@@ -8,29 +8,16 @@ const importsCanister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    ...getTests(createSnakeCaseProxy(importsCanister)),
-    {
-        name: 'get_math_message',
-        test: async () => {
-            const result = await importsCanister.get_math_message();
+// TODO add a test like this back in once we have a pip installable package to test on
+// {
+//     name: 'get_external_module_message',
+//     test: async () => {
+//         const result = await imports_canister.get_external_module_message();
 
-            return {
-                Ok: result === 11n
-            };
-        }
-    }
-    // TODO add a test like this back in once we have a pip installable package to test on
-    // {
-    //     name: 'get_external_module_message',
-    //     test: async () => {
-    //         const result = await imports_canister.get_external_module_message();
+//         return {
+//             ok: result === 3n
+//         };
+//     }
+// }
 
-    //         return {
-    //             ok: result === 3n
-    //         };
-    //     }
-    // }
-];
-
-runTests(tests);
+runTests(getTests(createSnakeCaseProxy(importsCanister)));
