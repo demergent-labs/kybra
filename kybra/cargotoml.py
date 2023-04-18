c14c431e505b1b41555e8bf59f80fd3ba1fe7581
Benjamin DeMann <benjamin.demann@gmail.com>
2023-04-18 11:09:04 -0600

    update rust python

--- kybra/cargotoml.py
+++ kybra/cargotoml.py
@@ -8,6 +8,7 @@ edition = "2018"
 [profile.release]
 opt-level = 'z'
 codegen-units = 1
+lto = true
 
 [lib]
 crate-type = ["cdylib"]
@@ -19,17 +20,17 @@ ic-cdk-timers = "0.1.2"
 candid = {{ version = "0.9.0-beta.2", features = ["parser"] }}
 kybra-vm-value-derive = {{ path = "./kybra_vm_value_derive" }}
 # TODO add this back once we support the full stdlib: https://github.com/demergent-labs/kybra/issues/12
-# rustpython = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false, features = ["stdlib", "freeze-stdlib"] }}
-rustpython = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false, features = ["stdlib"] }}
-rustpython-vm = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false }}
-rustpython-stdlib = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false, features = [] }}
-rustpython-derive = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false, features = [] }}
+# rustpython = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = ["stdlib", "freeze-stdlib"] }}
+rustpython = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = ["stdlib"] }}
+rustpython-vm = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = ["ic"] }}
+rustpython-stdlib = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = [] }}
+rustpython-derive = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = [] }}
 # rustpython = {{ path = "/home/RustPython", default-features = false, features = ["stdlib"] }}
-# rustpython-vm = {{ path = "/home/RustPython/vm", default-features = false }}
+# rustpython-vm = {{ path = "/home/RustPython/vm", default-features = false, features = ["ic"] }}
 # rustpython-stdlib = {{ path = "/home/RustPython/stdlib", default-features = false, features = [] }}
 # rustpython-derive = {{ path = "/home/RustPython/derive", default-features = false, features = [] }}
 # TODO add this back once we support the full stdlib: https://github.com/demergent-labs/kybra/issues/12
-# rustpython-pylib = {{ git = "https://github.com/demergent-labs/RustPython", rev = "7483ff7959103d7c4b2a15d718d4f7ad9b57b459", default-features = false, features = [] }}
+# rustpython-pylib = {{ git = "https://github.com/demergent-labs/RustPython", rev = "b7b0a4994d7871bf1e21fedb6bd0f0e5639fa874", default-features = false, features = [] }}
 # rustpython = {{ path = "../../../../../../RustPython", default-features = false, features = [] }}
 getrandom = {{ version = "0.2.3", features = ["custom"] }}
 serde = "1.0.137"
