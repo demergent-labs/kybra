330b65dbe675e6f59b8d897fe21a2dec5635f56f
Benjamin DeMann <benjamin.demann@gmail.com>
2023-04-28 15:43:38 -0600

    add annotated example

---
+++ examples/annotated_tests/src/main.py
@@ -0,0 +1,28 @@
+from typing import TypeAlias
+from kybra import Alias, Func, FuncTuple, nat8, Principal, query, StableBTreeMap, Query
+
+
+stable_map0: StableBTreeMap[nat8, str] = StableBTreeMap[nat8, str](
+    memory_id=0, max_key_size=100, max_value_size=100
+)
+
+
+BoolAlias: TypeAlias = Alias[bool]
+
+
+BasicFunc: type[FuncTuple] = Func(Query[[str], str])
+
+
+@query
+def is_empty() -> bool:
+    return stable_map0.is_empty()
+
+
+@query
+def get_type_alias() -> BoolAlias:
+    return True
+
+
+@query
+def get_func() -> BasicFunc:
+    return (Principal.from_str("aaaaa-aa"), "create_canister")
