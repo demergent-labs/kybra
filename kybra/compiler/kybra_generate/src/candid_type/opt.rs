5ffba932f061a5fb5fe915aaab102760f075ee04
Benjamin DeMann <benjamin.demann@gmail.com>
2023-05-04 10:37:59 -0600

    opt errors

--- kybra/compiler/kybra_generate/src/candid_type/opt.rs
+++ kybra/compiler/kybra_generate/src/candid_type/opt.rs
@@ -1,25 +1,25 @@
-use cdk_framework::act::node::candid::Array;
+use cdk_framework::act::node::candid::Opt;
 use rustpython_parser::ast::{ExprKind, Located};
 
 use crate::{source_map::SourceMapped, Error};
 
 impl SourceMapped<&Located<ExprKind>> {
-    pub fn is_array(&self) -> bool {
+    pub fn is_opt(&self) -> bool {
         match &self.node {
             ExprKind::Subscript { value, .. } => match &value.node {
-                ExprKind::Name { id, .. } => id == "Vec",
+                ExprKind::Name { id, .. } => id == "Opt",
                 _ => false,
             },
             _ => false,
         }
     }
 
-    pub fn to_array(&self) -> Result<Array, Vec<Error>> {
-        if !self.is_array() {
+    pub fn to_opt(&self) -> Result<Opt, Vec<Error>> {
+        if !self.is_opt() {
             return Err(crate::errors::unreachable().into());
         }
         match &self.node {
-            ExprKind::Subscript { slice, .. } => Ok(Array {
+            ExprKind::Subscript { slice, .. } => Ok(Opt {
                 enclosed_type: Box::from(
                     SourceMapped::new(slice.as_ref(), self.source_map.clone()).to_candid_type()?,
                 ),
--- kybra/compiler/kybra_generate/src/candid_type/opt/errors.rs
+++
@@ -1,9 +0,0 @@
-use rustpython_parser::ast::{ExprKind, Located};
-
-use crate::{errors::CreateMessage, source_map::SourceMapped, Error};
-
-impl SourceMapped<&Located<ExprKind>> {
-    pub fn not_opt_error(&self) -> Error {
-        Error::NotAnOpt(self.create_error_message("This is is not an Opt", "", None))
-    }
-}
