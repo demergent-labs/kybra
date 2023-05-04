cc1c65284a076a7a7739af5a08c7b47139718e3b
Benjamin DeMann <benjamin.demann@gmail.com>
2023-05-04 09:35:30 -0600

    update array errors

--- kybra/compiler/kybra_generate/src/candid_type/array/mod.rs
+++ kybra/compiler/kybra_generate/src/candid_type/array/mod.rs
@@ -1,5 +1,3 @@
-pub mod errors;
-
 use cdk_framework::act::node::candid::Array;
 use rustpython_parser::ast::{ExprKind, Located};
 
@@ -18,23 +16,14 @@ impl SourceMapped<&Located<ExprKind>> {
 
     pub fn to_array(&self) -> Result<Array, Vec<Error>> {
         if !self.is_array() {
-            return Err(vec![self.not_array_error()]);
+            return Err(crate::errors::unreachable().into());
         }
         match &self.node {
-            ExprKind::Subscript { value, slice, .. } => {
-                match &value.node {
-                    ExprKind::Name { id, .. } => {
-                        if id != "Vec" {
-                            return Err(vec![crate::errors::unreachable()]);
-                        }
-                    }
-                    _ => return Err(vec![crate::errors::unreachable()]),
-                }
-                let kybra_expr = SourceMapped::new(slice.as_ref(), self.source_map.clone());
-                Ok(Array {
-                    enclosed_type: Box::from(kybra_expr.to_candid_type()?),
-                })
-            }
+            ExprKind::Subscript { slice, .. } => Ok(Array {
+                enclosed_type: Box::from(
+                    SourceMapped::new(slice.as_ref(), self.source_map.clone()).to_candid_type()?,
+                ),
+            }),
             _ => Err(vec![crate::errors::unreachable()]),
         }
     }
