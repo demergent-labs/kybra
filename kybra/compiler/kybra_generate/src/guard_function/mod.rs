f69e8ce2d6a139c774135ab3da0830ae195bf816
Benjamin DeMann <benjamin.demann@gmail.com>
2023-05-09 21:55:39 -0600

    pr fixes

--- kybra/compiler/kybra_generate/src/guard_function/mod.rs
+++ kybra/compiler/kybra_generate/src/guard_function/mod.rs
@@ -1,23 +1,19 @@
 pub mod errors;
 mod guard_function;
 
-use cdk_framework::{
-    act::node::{CandidType, GuardFunction},
-    traits::CollectResults,
-};
+use cdk_framework::act::node;
 use rustpython_parser::ast::{ExprKind, Located, StmtKind};
 
-use crate::{
-    errors::{CollectResults as OtherCollectResults, Unreachable},
-    py_ast::PyAst,
-    source_map::SourceMapped,
-    Error,
-};
+use crate::{errors::CollectResults, py_ast::PyAst, source_map::SourceMapped, Error};
 
-use self::errors::{GuardFunctionParam, GuardFunctionReturn};
+use self::errors::GuardFunctionParam;
+
+struct GuardFunction<'a> {
+    name: &'a String,
+}
 
 impl PyAst {
-    pub fn build_guard_functions(&self) -> Result<Vec<GuardFunction>, Vec<Error>> {
+    pub fn build_guard_functions(&self) -> Result<Vec<node::GuardFunction>, Vec<Error>> {
         Ok(self
             .get_stmt_kinds()
             .iter()
@@ -30,68 +26,38 @@ impl PyAst {
 }
 
 impl SourceMapped<&Located<StmtKind>> {
-    pub fn is_guard_function(&self) -> bool {
-        if let StmtKind::FunctionDef { .. } = self.node {
-            match self.build_return_type() {
-                Ok(return_type) => {
-                    if let CandidType::TypeRef(type_ref) = return_type {
-                        return type_ref.name == "GuardResult";
+    fn get_guard_function(&self) -> Option<GuardFunction> {
+        if let StmtKind::FunctionDef { name, returns, .. } = &self.node {
+            if let Some(returns) = returns {
+                if let ExprKind::Name { id, .. } = &returns.node {
+                    if id == "GuardResult" {
+                        return Some(GuardFunction { name });
                     }
-                    false
                 }
-                Err(_) => false,
             }
-        } else {
-            false
         }
+        None
     }
 
-    pub fn as_guard_function(&self) -> Result<Option<GuardFunction>, Vec<Error>> {
-        if !self.is_guard_function() {
-            return Ok(None);
-        }
-        match &self.node {
-            StmtKind::FunctionDef { name, .. } => {
-                (
-                    match !self.has_params() {
-                        true => Ok(()),
-                        false => Err(GuardFunctionParam::err_from_stmt(self).into()),
-                    },
-                    match self.returns_guard_result() {
-                        true => Ok(()),
-                        false => Err(GuardFunctionReturn::err_from_stmt(self).into()),
-                    },
-                )
-                    .collect_results()?;
+    pub fn as_guard_function(&self) -> Result<Option<node::GuardFunction>, Vec<Error>> {
+        match self.get_guard_function() {
+            Some(guard_function) => {
+                if self.has_params() {
+                    return Err(GuardFunctionParam::err_from_stmt(self).into());
+                }
 
-                Ok(Some(GuardFunction {
-                    body: guard_function::generate(name),
-                    name: name.clone(),
+                Ok(Some(node::GuardFunction {
+                    body: guard_function::generate(guard_function.name),
+                    name: guard_function.name.clone(),
                 }))
             }
-            _ => return Err(Unreachable::error().into()),
+            None => Ok(None),
         }
     }
 
     fn has_params(&self) -> bool {
         match &self.node {
             StmtKind::FunctionDef { args, .. } => args.args.len() > 0,
-            _ => false,
-        }
-    }
-
-    fn returns_guard_result(&self) -> bool {
-        match &self.node {
-            StmtKind::FunctionDef { returns, .. } => {
-                if let Some(returns) = returns {
-                    match &returns.node {
-                        ExprKind::Name { id, .. } => id == "GuardResult",
-                        _ => false,
-                    }
-                } else {
-                    false
-                }
-            }
             _ => false,
         }
     }
