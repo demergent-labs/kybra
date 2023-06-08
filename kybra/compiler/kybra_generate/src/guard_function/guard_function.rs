d560ea44d064d5e0a907a311dd66f868d02da9d9
Dan Steren <dan@demergentlabs.org>
2023-06-08 12:23:08 -0600

    Abstract common functionality for guard functions

--- kybra/compiler/kybra_generate/src/guard_function/guard_function.rs
+++ kybra/compiler/kybra_generate/src/guard_function/guard_function.rs
@@ -3,38 +3,6 @@ use quote::quote;
 
 pub fn generate(function_name: &String) -> TokenStream {
     quote! {
-        unsafe {
-            let interpreter = INTERPRETER_OPTION
-                .as_mut()
-                .ok_or_else(|| "SystemError: missing python interpreter".to_string())?;
-            let scope = SCOPE_OPTION
-                .as_mut()
-                .ok_or_else(|| "SystemError: missing python scope".to_string())?;
-
-            interpreter.enter(|vm| {
-                scope
-                    .globals
-                    .get_item(#function_name, vm)
-                    .map_err(|err| {
-                        let py_object = err.to_pyobject(vm);
-                        let type_name = py_object.class().name().to_string();
-                        match py_object.str(vm) {
-                            Ok(err_message) => format!("{type_name}: {}", err_message.to_string()),
-                            Err(_) => format!("Attribute Error: '{type_name}' object has no attribute '__str__'"),
-                        }
-                    })?
-                    .call((), vm)
-                    .map_err(|err| {
-                        let py_object = err.to_pyobject(vm);
-                        let type_name = py_object.class().name().to_string();
-                        match py_object.str(vm) {
-                            Ok(err_message) => format!("{type_name}: {}", err_message.to_string()),
-                            Err(_) => format!("Attribute Error: '{type_name}' object has no attribute '__str__'"),
-                        }
-                    })?
-                    .try_from_vm_value(vm)
-                    .map_err(|vmc_err| vmc_err.0)?
-            })
-        }
+        call_global_python_function_sync(#function_name, ())?
     }
 }
