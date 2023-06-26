50e23a7a99651adb294e213a3c5b678a4aec70a6
Jordan Last <jordan.michael.last@gmail.com>
2023-06-26 14:29:10 -0500

    update the date tests to use stdlib datetime

--- examples/date/src/main.py
+++ examples/date/src/main.py
@@ -1,13 +1,60 @@
-# TODO Update this example to match the Azle date example more closely once we can support datetime
+from datetime import datetime
+from kybra import query
 
-from kybra import float64, query
-
-from time import gmtime, strftime, time
 
 @query
-def get_time() -> float64:
-    return time()
+def get_date(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).day
+
 
 @query
-def get_strftime() -> str:
-    return strftime("%a %b %d %Y", gmtime())
+def get_day(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).weekday()
+
+
+@query
+def get_full_year(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).year
+
+
+@query
+def get_hours(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).hour
+
+
+@query
+def get_milliseconds(isoString: str) -> int:
+    dt = datetime.fromisoformat(isoString)
+    return dt.microsecond // 1000
+
+
+@query
+def get_minutes(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).minute
+
+
+@query
+def get_month(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).month
+
+
+@query
+def get_seconds(isoString: str) -> int:
+    return datetime.fromisoformat(isoString).second
+
+
+@query
+def get_time(isoString: str) -> int:
+    dt = datetime.fromisoformat(isoString)
+    return int(dt.timestamp() * 1000)
+
+
+@query
+def get_timezone_offset(isoString: str) -> int:
+    dt = datetime.fromisoformat(isoString)
+    utcoffset = dt.utcoffset()
+
+    if utcoffset is None:
+        return 0
+    else:
+        return int(utcoffset.total_seconds() / 60) if dt.tzinfo else 0
--- examples/date/test/test.ts
+++ examples/date/test/test.ts
@@ -1,6 +1,6 @@
-import { createSnakeCaseProxy, runTests, Test } from 'azle/test';
-import { getTests } from 'azle/examples/date/test/tests';
+import { runTests, Test } from 'azle/test';
 import { createActor } from './dfx_generated/date';
+import { getTests } from './tests';
 
 const dateCanister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     agentOptions: {
@@ -8,32 +8,6 @@ const dateCanister = createActor('rrkah-fqaaa-aaaaa-aaaaq-cai', {
     }
 });
 
-const tests: Test[] = [
-    // ...getTests(createSnakeCaseProxy(dateCanister)),
-    {
-        name: 'get_time',
-        test: async () => {
-            const result = await dateCanister.get_time();
-
-            const seconds = new Date().getTime() / 1_000;
-
-            return {
-                Ok: seconds - result < 10
-            };
-        }
-    },
-    {
-        name: 'get_strftime',
-        test: async () => {
-            const result = await dateCanister.get_strftime();
-
-            const now = new Date().toDateString();
-
-            return {
-                Ok: result === now
-            };
-        }
-    }
-];
+const tests: Test[] = getTests(dateCanister);
 
 runTests(tests);
