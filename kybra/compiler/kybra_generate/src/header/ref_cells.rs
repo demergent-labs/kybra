eda791e44ec130d45418e24d4ef80fc67f56de4b
Jordan Last <jordan.michael.last@gmail.com>
2023-06-28 14:07:36 -0500

    remove unwraps from ref_cells

--- kybra/compiler/kybra_generate/src/header/ref_cells.rs
+++ kybra/compiler/kybra_generate/src/header/ref_cells.rs
@@ -15,7 +15,7 @@ pub fn generate() -> TokenStream {
             > = std::cell::RefCell::new(
                 ic_stable_structures::cell::Cell::init(
                     MEMORY_MANAGER_REF_CELL.with(|m| m.borrow().get(ic_stable_structures::memory_manager::MemoryId::new(#PYTHON_STDLIB_MEMORY_ID))), vec![]
-                ).unwrap()
+                ).unwrap_or_trap()
             );
 
             static CANISTER_INITIALIZED_REF_CELL: std::cell::RefCell<
@@ -28,7 +28,7 @@ pub fn generate() -> TokenStream {
             > = std::cell::RefCell::new(
                 ic_stable_structures::cell::Cell::init(
                     MEMORY_MANAGER_REF_CELL.with(|m| m.borrow().get(ic_stable_structures::memory_manager::MemoryId::new(#CANISTER_INITIALIZED_MEMORY_ID))), 0
-                ).unwrap()
+                ).unwrap_or_trap()
             );
 
             static RANDOMNESS_STABLE_REF_CELL: std::cell::RefCell<
@@ -41,7 +41,7 @@ pub fn generate() -> TokenStream {
             > = std::cell::RefCell::new(
                 ic_stable_structures::cell::Cell::init(
                     MEMORY_MANAGER_REF_CELL.with(|m| m.borrow().get(ic_stable_structures::memory_manager::MemoryId::new(#RANDOMNESS_MEMORY_ID))), vec![]
-                ).unwrap()
+                ).unwrap_or_trap()
             );
         }
     }
