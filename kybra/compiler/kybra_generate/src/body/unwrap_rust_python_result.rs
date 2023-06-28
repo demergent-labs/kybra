5aa11a16d0ce04602552863b0ac51994267b8994
Jordan Last <jordan.michael.last@gmail.com>
2023-06-28 14:07:36 -0500

    remove unwraps from ref_cells

--- kybra/compiler/kybra_generate/src/body/unwrap_rust_python_result.rs
+++ kybra/compiler/kybra_generate/src/body/unwrap_rust_python_result.rs
@@ -52,6 +52,15 @@ pub fn generate() -> TokenStream {
             }
         }
 
+        impl<T> UnwrapOrTrap<T> for Result<T, ic_stable_structures::cell::InitError> {
+            fn unwrap_or_trap(self) -> T {
+                match self {
+                    Ok(ok) => ok,
+                    Err(err) => ic_cdk::trap(&init_error_to_string(&err)),
+                }
+            }
+        }
+
         pub trait UnwrapOrTrapWithVm<T> {
             fn unwrap_or_trap(self, vm: &rustpython::vm::VirtualMachine) -> T;
         }
@@ -71,6 +80,18 @@ pub fn generate() -> TokenStream {
                         };
                         ic_cdk::trap(format!("{type_name}: {err_message}").as_str())
                     }
+                }
+            }
+        }
+
+        fn init_error_to_string(err: &ic_stable_structures::cell::InitError) -> String {
+            match err {
+                ic_stable_structures::cell::InitError::IncompatibleVersion {
+                    last_supported_version,
+                    decoded_version,
+                } => format!("InitError: IncompatibleVersion, last_supported_version {last_supported_version}, decoded_version {decoded_version}"),
+                ic_stable_structures::cell::InitError::ValueTooLarge { value_size } => {
+                    format!("InitError: ValueTooLarge {value_size}")
                 }
             }
         }
